de516cda03b5963250fc52f313646f334d266ddf
sk saud <soudshaik18@gmail.com>
2022-03-08 10:09:05 +0530

    First commit

---
+++ E-commerceWebsiteforSportyShoes/src/main/java/com/simplilearn/ECommerceWebsiteforSportyShoesApplication.java
@@ -0,0 +1,15 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceWebsiteforSportyShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceWebsiteforSportyShoesApplication.class, args);
+	}
+
+}
---
+++ E-commerceWebsiteforSportyShoes/src/main/java/com/simplilearn/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.simplilearn.repository;
+
+
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.simplilearn.model.User;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+Optional<User>findUserByEmail(String email);
+
+
+}
